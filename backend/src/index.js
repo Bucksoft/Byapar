dbc69c6214877396d74080d674c6356c647cca99
Bucksoftech <bucksoftechpvtltd@gmail.com>
2025-09-03 12:53:57 +0530

    Fixed path of frontend/dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,7 +44,7 @@ app.use("/api/v1/quotation", quotationRoutes);
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("/*any", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.listen(PORT, () => {
