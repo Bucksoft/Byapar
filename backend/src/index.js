cdce8b43d133a06b56310de1eecc3b3551163c83
Bucksoftech <bucksoftechpvtltd@gmail.com>
2025-09-03 11:54:38 +0530

    Fix _dirname import in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,6 +41,7 @@ app.use("/api/v1/sales-invoice", salesRoutes);
 app.use("/api/v1/generate-pdf", pdfRoutes);
 app.use("/api/v1/quotation", quotationRoutes);
 
+const _dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
