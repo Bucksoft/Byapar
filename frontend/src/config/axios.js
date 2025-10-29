aa634a475dc927037faa7de4ce896d146b5dff64
Bucksoftech <bucksoftechpvtltd@gmail.com>
2025-10-29 19:16:36 +0530

    Fixed api url in frontedn

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL || "https://backend.byaparsetu.com";
 export const axiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`,
   withCredentials: true,
